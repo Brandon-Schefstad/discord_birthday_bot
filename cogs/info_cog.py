c6be1d2bcf97fffb718bbe5122e4e566f7e3bf56
Brandon-Schefstad <bschefstad@gmail.com>
2024-03-04 21:06:50 -0500

    Dont need !scan anymore

--- cogs/info_cog.py
+++ cogs/info_cog.py
@@ -11,27 +11,37 @@ from datetime import date
 class info_cog(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
-        print("Starting up Info cog!")
+        print(f'{self.guild_id}')
 
     @commands.Cog.listener()
     async def on_ready(self):
-        pass
-
-    @commands.command()
-    async def scan(self, ctx):
-        room = ctx.guild
+        room = self.bot.guilds[1]
         print(
             f"""
         Name of server: {room}
         Server ID: {room.id}
-        Current channel: {ctx.channel}
         Current number of members: {room.member_count}
         Member List: {room.members.__dict__}
         Available channels:{room.text_channels}
-        Message from: {ctx.message.author}
-        Author ID: {ctx.message.author.id}
-        """
-        )
+        """)
+        pass
+
+    # @commands.command()
+    # async def scan(self, ctx):
+    #     room = ctx.guild
+    #     print(
+    #         f"""
+    #     Name of server: {room}
+    #     Server ID: {room.id}
+    #     Current channel: {ctx.channel}
+    #     Current number of members: {room.member_count}
+    #     Member List: {room.members.__dict__}
+    #     Available channels:{room.text_channels}
+    #     Available channels:{room.voice_channels}
+    #     Message from: {ctx.message.author}
+    #     Author ID: {ctx.message.author.id}
+    #     """
+    #     )
 
 
 async def setup(bot):
